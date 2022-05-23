223cde078ca2186bb023d5ee178be037fbd3112d
Morgan Wilde <morgan@wilde.mx>
2022-05-23 15:00:00 +1000

    Fixed with awaits and added try catch

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -15,45 +15,32 @@ function Weather() {
   const [loading, setLoading] = useState(true)
 
   const getIP = async () => {
-    fetch('https://ipapi.co/json/')
-      .then((response) => {
-        return response.json()
-      })
-      .then((res) => {
-        localStorage.setItem('location', res.ip)
-      })
-      .catch((err) => console.error('Problem fetching my IP', err))
+    try {
+      if (!location) {
+        const response = await fetch('https://ipapi.co/json/')
+        const json = await response.json()
+        localStorage.setItem('location', json.ip)
+      }
+      getWeather()
+    } catch (error) {
+      console.error('Problem fetching my IP', error)
+    }
   }
   const getWeather = async () => {
-    fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API}&q=${location}&days=3&aqi=no&alerts=no`
-    )
-      .then((response) => {
-        return response.json()
-      })
-      .then((res) => {
-        localStorage.setItem('weather', JSON.stringify(res))
-      })
+    try {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API}&q=${location}&days=3&aqi=no&alerts=no`
+      )
+      const json = await response.json()
+      localStorage.setItem('weather', JSON.stringify(json))
+      setLoading(false)
+    } catch (error) {
+      console.error('Problem fetching weather', error)
+    }
   }
 
   useEffect(() => {
-    if (!location) {
-      getIP()
-    }
-    if (localStorage.getItem('weather')) {
-      // Check if local is 30 minutes old data
-      let current = Math.floor(Date.now() / 1000)
-      let time = JSON.parse(localStorage.getItem('weather')).location
-        .localtime_epoch
-      if (current - time > 1800000) {
-        console.log(time)
-        getWeather()
-      }
-    } else {
-      getWeather()
-    }
-    console.log(localStorage.getItem('weather'))
-    setLoading(false)
+    getIP()
   }, [])
 
   if (loading) {
