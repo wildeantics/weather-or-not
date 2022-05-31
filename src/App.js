f7a95a32898633ebe4a19f81ddbc88e7945e27a4
Morgan Wilde <morgan@wilde.mx>
2022-05-31 14:15:44 +1000

    Added toastify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import About from './pages/About'
 import Weather from './pages/Weather'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
@@ -11,6 +13,18 @@ function App() {
           <Route path='/about' element={<About />} />
         </Routes>
       </Router>
+
+      <ToastContainer
+        position='top-center'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   )
 }
